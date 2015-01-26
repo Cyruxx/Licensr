e69485e828f322737ce7d0d03d2d7a182ffcaa14
Phillipp Ohlandt <phillipp.ohlandt@gmail.com>
2015-01-26 18:11:50 +0100

    db config

---
+++ env-example.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * rename it to .env.php
+ * fill in your data
+ * */
+
+return array(
+
+    'DB_HOST' => '',
+    'DB_DATABASE' => '',
+    'DB_USERNAME' => '',
+    'DB_PW' => '',
+);
